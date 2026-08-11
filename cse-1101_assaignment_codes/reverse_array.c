ea877be3ec36312e99159dd87f08c8ac5992c35f
mugdha-sarker81 <mugdhasarker81@gmail.com>
2026-08-12 01:42:42 +0600

    Create reverse_array.c

---
+++ cse-1101_assaignment_codes/reverse_array.c
@@ -0,0 +1,26 @@
+/*
+print an array in reverse order
+*/
+#include<stdio.h>
+ void   array_in(int,int *ar[]);
+  void  array_rev_out(int,int *ar[]);
+
+int main()
+{
+    int size; scanf("%d",&size);
+    int arr[size];
+    array_in(size,arr);
+    array_rev_out(size,arr);
+}
+
+void array_in(int n,int *ar[n]){
+    for(int i=0;i<n;i++){
+        scanf("%d",&ar[i]);
+    }
+}
+
+void array_rev_out(int n,int *ar[]){
+    for(int i=n-1;i>=0;i--){
+        printf("%d ",ar[i]);
+    }
+}
