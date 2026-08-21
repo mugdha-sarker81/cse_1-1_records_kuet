b987a0b7526f3f464d1491fd1092dcbbdb3e4fe5
mugdha-sarker81 <mugdhasarker81@gmail.com>
2026-08-21 21:58:20 +0600

    Add three_threes.cpp for AtCoder problem
    Implement main function for three_threes problem.

---
+++ SGIPC_contests/TFC-6/three_threes.cpp
@@ -0,0 +1,14 @@
+/*
+    https://atcoder.jp/contests/abc333/tasks/abc333_a?lang=en
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n; cin>>n;
+    for(int i=0;i<n;i++){
+        cout<<n;
+    }
+    return 0;
+}
